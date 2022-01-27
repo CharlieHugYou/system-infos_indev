ba7d42a099f062ff40486ae765a23943b59e7b51
Charlie <91826730+CharlieHugYou@users.noreply.github.com>
2022-01-27 23:32:44 +0100

    Add dev function

--- System-Infos/Main.cpp
+++ System-Infos/Main.cpp
@@ -3,23 +3,76 @@
 #include "pass.h"
 #include "settings.h"
 #include "Main.h"
+#include "utilities.h"
+
+void dev();
 
 int main()
 {
-	checkFirstConnection();
+	dev();
+
+	system("cls");
+	/*load();
+	checkFirstConnection();*/
 
 	return 0;
 }
 
 void start()
 {
-	std::cout << "Start function" << std::endl;
 	menu();
 }
 
 void menu()
 {
-	std::string str;
-	std::cin >> str;
-	std::cout << str;
+	std::string userInput;
+
+	std::cout << "Menu principale" << std::endl
+		<< "| -- > 1. Acceder au menu des commande" << std::endl
+		<< "| -- > 2. Parametre" << std::endl
+		<< "| -- > 3. Quitte" << std::endl
+		<< "Commande rapide : /help" << std::endl;
+
+	std::cin >> userInput;
+
+	if (userInput == "1" || "command")
+	{
+		command();
+	}
+
+	if (userInput == "2" || "set" || "settings" || "parametre")
+	{
+		writeSettings();
+	}
+
+	if (userInput == "3" || "quitte")
+	{
+		exit(1);
+	}
+
+	if (userInput == "/help")
+	{
+		system("cls");
+		help_command("help.data.show");
+	}
+}
+
+void command()
+{
+	system("cls");
+	std::cout << "Type a command" << std::endl;
+}
+
+
+// pour tester la fonction de test et les autres fonction qui arriveront et que je dois tester en dehors d'un ajout dans un code déjà présent
+void dev()
+{
+	while (true)
+	{
+		text("test", "red", "stay");
+		text("test2", "", "jump-line");
+		text("test3", "green", "stay");
+		break;
+	}
+	std::cin.ignore();
 }
