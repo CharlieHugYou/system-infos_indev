fbbfd25d6338f3c524354db0370db25995c5e10c
Charlie <91826730+CharlieHugYou@users.noreply.github.com>
2022-01-30 00:32:43 +0100

    Update utilities.cpp
    Fin du développement de la fonction text, fin du développement de la fonction

--- System-Infos/utilities.cpp
+++ System-Infos/utilities.cpp
@@ -1,6 +1,7 @@
 #include "utilities.h"
 
 #include <fstream>
+#include <string>
 
 void help_command(std::string file_path) 
 {
@@ -11,12 +12,15 @@ void help_command(std::string file_path)
 
 	if (file.is_open())
 	{
-		while (file >> stored_help_file_content)
+		while (std::getline(file, stored_help_file_content))
 		{
 			std::cout << stored_help_file_content << std::endl;
 
 		}
+		file.close();
 	}
+
+	std::cout << std::endl;
 }
 
 void write_logs(std::string logs_to_write)
@@ -30,38 +34,48 @@ void read_logs()
 	// to code
 }
 
+// couleur pour la fonction text
 #define RED "\x1B[31m"
 #define GRE "\x1B[32m"
+#define WHI "\033[0m\t\t"
 
-void text(std::string text_to_show, std::string text_color, std::string action)
+std::string text(std::string text_to_show, std::string text_color, std::string action)
 {
 	if (text_color == "" && action == "jump-line") // saute une ligne
 	{
-		std::cout << text_to_show << std::endl;
+		std::cout << text_to_show << WHI << std::endl;
 	}
 	else if (text_color == "" && action == "stay") // ne saute pas de ligne
 	{
-		std::cout << text_to_show;
+		std::cout << text_to_show << WHI;
 	}
 
 
 	if (text_color == "red" && action == "jump-line")
 	{
-		std::cout << RED << text_to_show << std::endl;
+		std::cout << RED << text_to_show << WHI << std::endl;
 	}
 	else if (text_color == "red" && action == "stay")
 	{
-		std::cout << RED << text_to_show;
+		std::cout << RED << text_to_show << WHI;
 	}
 
 
 	if (text_color == "green" && action == "jump-line")
 	{
-		std::cout << GRE << text_to_show << std::endl;
+		std::cout << GRE << text_to_show << WHI << std::endl;
 	}
 	else if (text_color == "green" && action == "stay")
 	{
-		std::cout << GRE << text_to_show;
+		std::cout << GRE << text_to_show << WHI;
 
 	}
+
+	if (action == "")
+	{
+		system("cls");
+		std::cout << "ERREUR : action param text funtion is NULL" << std::endl;
+	}
+
+	return "";
 }
