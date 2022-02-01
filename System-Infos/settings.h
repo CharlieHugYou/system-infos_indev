c7d5dfe0f7030b6110270e74cfbddaf3d48e11aa
Charlie <91826730+CharlieHugYou@users.noreply.github.com>
2022-02-01 17:25:02 +0100

    Add user struct

--- System-Infos/settings.h
+++ System-Infos/settings.h
@@ -1,13 +1,22 @@
 #pragma once
 
+#include <iostream>
+
 // in settings.cpp file
-void writeSettings();
+void settingsMenu();
+
+void writeSettings(int goto_menu);
 void writeSettingsConfigFirstConnect();
 
 void readSettingsFile();
 
 void checkFirstConnection();
 void firstConnectProcess();
+
+struct user
+{
+	std::string name;
+};
 
 // in sreen-loader.cpp file
 void load();
