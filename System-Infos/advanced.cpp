7c46fb17627bb355b897a089af83edcbf83cb487
Charlie <91826730+CharlieHugYou@users.noreply.github.com>
2022-02-06 10:59:49 +0100

    Add files via upload

---
+++ System-Infos/advanced.cpp
@@ -0,0 +1,29 @@
+#include "advanced.h"
+#include "utilities.h"
+#include "settings.h"
+
+#include <iostream>
+#include <fstream>
+
+void activate()
+{
+	user user;
+
+	std::string userInput;
+	text("Pour acceder a toutes les commmandes de System-Infos, tu dois activer toutes les commandes", "white", "jump-line");
+	text("Ecris active pour active toutes les commandes", "write", "jumpe-line");
+
+	std::cin >> userInput;
+
+	if (userInput == "active")
+	{
+		std::ofstream read_user_file("user.data");
+
+		read_user_file << user.name << "\n" << "true";
+	}
+}
+
+/*
+	si je fais un truc pour avoir le userName, c'est pour pouvoir écrire l'activation des commandes avancé 
+	et ne prendre en compte le userName
+*/
